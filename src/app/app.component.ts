cc0d79ed33b6b3e9529ba5668dd1b2134ae80300
Manoj Barman <the_manoj@live.com>
2022-01-08 11:50:35 +0530

    SW update + dark mode support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { ApplicationRef, Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { interval } from 'rxjs';
 import { DataService, User } from './services/data.service';
 
 @Component({
@@ -12,11 +14,46 @@ export class AppComponent implements OnInit {
 
   users: User[] = [];
 
-  constructor(private data: DataService) { }
+  constructor(
+    private data: DataService,
+    private swUpdate: SwUpdate,
+    private app: ApplicationRef
+  ) {
+    this.updateApp();
+    this.checkForUpdate();
+  }
 
   ngOnInit(): void {
     this.data.getUsers()
       .subscribe(users => this.users = users);
+  }
+
+  updateApp() {
+    if (!this.swUpdate.isEnabled) {
+      console.log('SW update not Enabled 🙁');
+      return
+    }
+    this.swUpdate.available.subscribe((event) => {
+      if (confirm('New version available. Would you like to update?')) {
+        this.swUpdate.activateUpdate().then(() => location.reload())
+      }
+    })
+    this.swUpdate.activated.subscribe((event) => {
+      console.log('Previous: ' + event.previous, ' Current: ' + event.current);
+    })
+  }
+
+  checkForUpdate() {
+    this.app.isStable.subscribe((stable) => {
+      if (stable) {
+        const timeInterval = interval(12 * 60 * 60 * 1000);
+        timeInterval.subscribe(() => {
+          this.swUpdate.checkForUpdate().then(() => {
+            console.log('Checked for latest app updates.');
+          })
+        })
+      }
+    })
   }
 
 }
